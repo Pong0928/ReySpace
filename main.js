037b59f84ee1265c6cd836b9051ce9aa479333b8
Rey <golingping@gmail.com>
2023-11-15 17:21:30 -0500

    prodtion

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburgerBtn").addEventListener("click", function() {
+    document.getElementById("mobileNav").classList.toggle("-top-32");
+    document.getElementById("mobileNav").classList.toggle("top-full");
+});
